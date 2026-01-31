26754d4e574cc7b5a346147ead762b85d1b4c63a
Mokhwa96 <mokhwa96@gmail.com>
2026-01-31 16:25:25 +0900

    Initial Spring Boot cat page

---
+++ src/main/java/com/example/catpage/CatPageApplication.java
@@ -0,0 +1,11 @@
+﻿package com.example.catpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatPageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CatPageApplication.class, args);
+    }
+}
